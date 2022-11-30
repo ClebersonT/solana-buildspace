c5a8482588d3e2c963f8bce66a1978698c8e6a28
Cleberson Teodoro <clebersonteodoro2015@hotmail.com>
2022-11-30 13:34:15 -0400

    getBalance e getAccountInfo

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import AddressForm from '../components/AddressForm'
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
+  const [isExecutable, setIsExecutable] = useState(false);
 
   const addressSubmittedHandler = (address: string) => {
     const key = new web3.PublicKey(address);
@@ -21,6 +22,13 @@ const Home: NextPage = () => {
       .then(balance => {
         setBalance(balance / web3.LAMPORTS_PER_SOL)
       })
+
+      connection.getAccountInfo(key)
+      .then(info => {
+        setIsExecutable(info?.executable ?? false);
+        console.log(isExecutable)
+      })
+
     } catch (error) {
       setAddress('')
       setBalance(0)
@@ -37,6 +45,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is it executable? ${isExecutable ? 'Yep' : 'Nope'}`}</p>
       </header>
     </div>
   )
